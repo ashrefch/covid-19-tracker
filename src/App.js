59e24e16f31ca704619d1703cd1c40440f8c78bd
ashrefch <69974761+ashrefch@users.noreply.github.com>
2020-11-11 11:15:34 +0100

    1st commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+CardContent} from "@material-ui/core"
+import React,{ useState,useEffect } from "react";
+import InfoBox from './InfoBox';
+import Map from './map';
+
+
+import './App.css';
+import Table from './tab.js';
+import { sortData } from "./util";
+import LineGraph from "./LineGraph";
+
+function App() {
+  const [countries,setCountries] = useState([]);
+  const [country,setCountry] =  useState('Worldwide');
+  const [countryInfo,setCountryInfo]= useState({});
+  const [tableData, setTableData]= useState([]);
+
+  useEffect(()=>{
+     fetch("https://disease.sh/v3/covid-19/all")
+    .then((response)=>response.json())
+    .then(data=>{
+      setCountryInfo(data);
+    })
+  },[]);
+
+  
+  useEffect(()=>{
+    const getCountriesData = async()=>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=> response.json())
+      .then((data)=>{
+        const countries = data.map((country)=>({
+          name: country.country,
+          value : country.countryInfo.iso2
+        }));
+        const sortedData = sortData(data);
+        setTableData(sortedData);
+        setCountries(countries);
+        
+      });
+    };
+    getCountriesData();
+  },[]);
+
+  const onCountryChange= async(event)=>{
+    const countryCode=  event.target.value;
+    
+    const url= countryCode === "worldwide" ? "https://disease.sh/v3/covid-19/countries" : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+      setCountry(countryCode);
+      console.log('country info is ',countryInfo);
+      setCountryInfo(data);
+    })
+  }
+
+  return (
+    <div className="app">
+     <div className="app__left">
+
+     <div className="app__header">
+     <h1>COVID-19 TRACKER </h1>
+      <FormControl className="app__dropdown">
+      <Select
+        variant="outlined" value={country} onChange={onCountryChange} >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        {countries.map((country)=>(
+          <MenuItem value={country.value}>{country.name}</MenuItem>
+        ))}
+
+      </Select>
+      </FormControl>   
+</div>
+
+          <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+          
+          </div>
+          <Map/>
+      
+
+    </div>
+          <Card className="app__right">
+          <CardContent>
+            <h3>Live cases by country</h3>
+            <Table countries={tableData}/>
+            <h3>Worldwide new cases</h3>
+            <LineGraph/> 
+          </CardContent>
+          </Card>
+
+     </div>
+
+  );
+        }
+export default App;
---
+++ src/map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function map() {
+    return (
+        <h2>I am a map</h2>
+    )
+}
+
+export default map
